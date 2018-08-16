60926b5b4c34f7eb76610d1e6d63e12f4ec0ea23
Sagar Choudhary <sagar.choudhary96@gmail.com>
2018-08-16 23:18:51 +0530

    First Example Initial Commit

---
+++ startup_namer/README.md
@@ -0,0 +1,8 @@
+# startup_namer
+
+A simple mobile app that generates proposed names for a startup company.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ startup_namer/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Startup Name Generator'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}

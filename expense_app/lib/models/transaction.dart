371ec2720ccaf2616a405766c67d128efcab50d2
Michael Dadzie <dadziemikke2@gmail.com>
2020-10-02 18:18:05 +0000

    Added Code

---
+++ expense_app/lib/models/transaction.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/foundation.dart';
+
+class Transaction {
+  String id;
+  String title;
+  double amount;
+  DateTime date;
+
+  Transaction(
+      {@required this.id,
+      @required this.title,
+      @required this.amount,
+      @required this.date});
+}
---
+++ expense_app/lib/widgets/adaptive_flat_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io';
+
+class AdaptiveFlatButton extends StatelessWidget {
+  final String text;
+  final Function handler;
+
+  AdaptiveFlatButton(this.text, this.handler);
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? CupertinoButton(
+            child: Text(
+              text,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            onPressed: handler,
+          )
+        : FlatButton(
+            textColor: Theme.of(context).primaryColor,
+            child: Text(
+              text,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            onPressed: handler,
+          );
+  }
+}
